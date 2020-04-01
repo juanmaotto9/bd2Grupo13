1663f803a1130acef8a0113c73df22c35c4a45e6
Juanma <Juanmaotto9@gmail.com>
2020-04-01 19:31:37 -0300

    Creadas las clases que me tocaron

---
+++ bdlivery-master/src/main/java/ar/edu/unlp/info/bd2/model/State.java
@@ -0,0 +1,18 @@
+package ar.edu.unlp.info.bd2.model;
+
+public class State {
+
+	private Integer id;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String myClass() {
+		return this.getClass().toString();
+	}
+
+}
