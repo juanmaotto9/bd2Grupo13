09ddfb3f8c947cff616100063656ee26abaea823
maruPortal <mariadelosangelesportal@gmail.com>
2020-04-27 16:39:10 -0300

    agregue script createdatabase.sh

--- bdlivery-master/src/main/java/ar/edu/unlp/info/bd2/config/HibernateConfiguration.java
+++ bdlivery-master/src/main/java/ar/edu/unlp/info/bd2/config/HibernateConfiguration.java
@@ -32,8 +32,8 @@ public class HibernateConfiguration {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/bd2_grupo" + this.getGroupNumber() + "?useSSL=false");
-        dataSource.setUsername("root");
-        dataSource.setPassword("bd2");
+        dataSource.setUsername("bd2_g13");
+        dataSource.setPassword("grupo13");
 
         return dataSource;
     }
