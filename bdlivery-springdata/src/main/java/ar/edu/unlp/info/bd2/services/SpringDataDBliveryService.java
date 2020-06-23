977e7f3c54365001e55b0b4b3886c01b1fcbd565
maruPortal <mariadelosangelesportal@gmail.com>
2020-06-23 01:28:10 -0300

    archivos base - SpringData

---
+++ bdlivery-springdata/src/main/java/ar/edu/unlp/info/bd2/services/SpringDataDBliveryService.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.info.bd2.services;
+
+import ar.edu.unlp.info.bd2.model.*;
+import ar.edu.unlp.info.bd2.repositories.*;
+
+public class SpringDataDBliveryService implements DBliveryService {
+	
+/*
+ 	private DBliveryRepository repository;
+	
+	public DBliveryServiceImpl(DBliveryRepository repository) {
+		this.repository = repository;
+	}
+
+ */
+}
